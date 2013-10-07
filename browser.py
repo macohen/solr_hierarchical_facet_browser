ea954e908db1defb1f51f4c7c1146101c4e19543
Mark Cohen <mcohen74@gmail.com>
2013-10-07 11:40:58 -0400

    refactored for clarity, modularity

--- browser.py
+++ browser.py
@@ -19,7 +19,6 @@ def group_facets(depth):
     q = SolrQuery()
     return q.query("*:*", facet='on', facet_field=['groups_s_mv'], facet_prefix=depth, rows=10, facet_mincount=1)
 
-
 def docs_with_facet(field):
     q = SolrQuery()
     return q.query("groups_s_mv:\"" + field + "\"", facet='on', facet_field=['file_name_s'], facet_mincount=1, rows=10)
@@ -57,67 +56,63 @@ def make_hier(group):
     """ copied from BVIToSolrConverter.py """
     group_parts = group.split(":")
     group = "/".join(group_parts)
-    group = str(len(group_parts)) + "/" + group  
+    group = str(len(group_parts)) + "/" + group 
     
     return group
 
-#def convert(group):
-    
+@app.template_filter('partial_hierarchy')
+def add_slash(group):
+    return group + "/"
+
 @app.route('/')
 def query():
     drill_down = "0/"
     if request.args.get('drill') != None:
         drill_down = request.args.get('drill')
-#        app.logger.debug("drill=" + drill_down)
     
     results = group_facets(drill_down)
-#    num_facets = len(results.facets['facet_fields'])
-#    if num_facets == 0:
-#        app.logger.debug("no more to drill down")
-#    else:
-#        app.logger.debug(str(num_facets) + " more to drill down")
-        
     facet_fields = {}
+    browse = False
+    # for each facet field
     for field, facets in results.facets['facet_fields'].iteritems():
-        i=0
-        name = ""
-        value = 0
-        pair=()
-        pair_list = []
-        app.logger.debug("facets: {0}".format(facets))
-        num_facets = len(facets)
-        if num_facets > 0:
-            for i in range(0, num_facets):
-                #            app.logger.debug(facets[i])
-                if i % 2 == 0:                
-                    name = facets[i]
-                    app.logger.debug("name= " + str(name))
-                else:
-                    value = facets[i]
-                    pair = (name, value)
-                    pair_list.append(pair)
-                    #               app.logger.debug("value= " + str(value))
-                    name = ""
-                    value = 0
-                    #              app.logger.debug(pair)
-                    pair = ()
+        facet_pair = build_facet_pairs(facets)
+        if len(facet_pair) > 0:
+            browse = True
+        facet_fields[field] = facet_pair
+
+    if browse == False:
+        app.logger.debug("browse off")
+        drill_up = drillup(drill_down)
+        drill_up = drill_up[0:len(drill_up)-1]
+        results = docs_with_facet(drill_up)
+    
+    if browse == True:
+        return render_template('browser.html', facets=facet_fields)
+    else:
+        return render_template('results.html', results=results)
+
+def build_facet_pairs(facets):
+    name = ""
+    value = 0
+    pair = ()
+    pair_list = []
+    num_facets = len(facets)
+    app.logger.debug(num_facets)
+    for i in range(0, num_facets):
+        app.logger.debug(facets[i])
+        if i % 2 == 0:                
+            name = facets[i]
+            app.logger.debug("name= " + str(name))
         else:
-            #back up one level
-            drill_up = drillup(drill_down)
-            results = docs_with_facet(drill_up)
-#            for result in results:
-#                app.logger.debug(result)
-
-            return render_template('results.html', results=results)
-            
-
- #       for item in pair_list:
- #           app.logger.debug(item)
-        facet_fields[field] = pair_list
- #       app.logger.debug(facet_fields)
-#        app.logger.debug(facet)
-#        app.logger.debug(type(facet))
-    return render_template('browser.html', facets=facet_fields)
+            value = facets[i]
+            pair = (name, value)
+            pair_list.append(pair)
+            #               app.logger.debug("value= " + str(value))
+            name = ""
+            value = 0
+            #              app.logger.debug(pair)
+            pair = ()
+    return pair_list
 
 if __name__ == '__main__':
     app.run()
